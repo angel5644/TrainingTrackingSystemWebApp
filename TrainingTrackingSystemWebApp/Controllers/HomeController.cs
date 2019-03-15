d48b1e588cf92ebf8452d6540947827859467bad
Carlos Omar Sanchez Solorzano <carlos.sanchez@4thsource.com>
2019-03-15 16:39:51 -0600

    first commit to push new branch

--- TrainingTrackingSystemWebApp/Controllers/HomeController.cs
+++ TrainingTrackingSystemWebApp/Controllers/HomeController.cs
@@ -16,7 +16,6 @@ namespace TrainingTrackingSystemWebApp.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
