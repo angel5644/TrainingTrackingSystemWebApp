ba8059a58ad35fe2eb3b56a5503fe4a8fb5e56d0
Luis Gerardo Galindo Luna <lgalindo2@ucol.mx>
2019-03-20 15:39:44 -0600

    example of filtering users

--- TrainingTrackingSystemWebApp/Controllers/HomeController.cs
+++ TrainingTrackingSystemWebApp/Controllers/HomeController.cs
@@ -8,9 +8,58 @@ namespace TrainingTrackingSystemWebApp.Controllers
 {
     public class HomeController : Controller
     {
+        public class UsersIndexViewModel
+        {
+            public List<UserViewModel> Users { get; set; }
+        }
+
+        public class UserViewModel
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Email { get; set; }
+            public int Type { get; set; }
+        }
+
         public ActionResult Index()
         {
-            return View();
+            UsersIndexViewModel viewModel = new UsersIndexViewModel();
+
+            // Call rest service
+            viewModel.Users = new List<UserViewModel>()
+            {
+                new UserViewModel()
+                {
+                    FirstName = "Victor",
+                    LastName = "Leon"
+                },
+                new UserViewModel()
+                {
+                    FirstName = "Eder",
+                    LastName = "Leon"
+                },
+            };
+
+            return View("Index", viewModel);
+        }
+
+        [HttpGet]
+        public JsonResult FindUsers(string searchField, string searchValue)
+        {
+            // Call rest service to filter users
+            List<UserViewModel> usersFiltered = new List<UserViewModel>()
+            {
+                new UserViewModel()
+                {
+                    FirstName = "Victor",
+                    LastName = "Leon"
+                }
+            };
+
+            return Json(
+                new { data = usersFiltered },
+                JsonRequestBehavior.AllowGet
+                );
         }
 
         public ActionResult About()
